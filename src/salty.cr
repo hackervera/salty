473d610c43017b1521d8b97fc66f53da7fbee18b
Tyler Gillies <tjgillies@gmail.com>
2016-12-25 20:46:20 -0800

    save game

--- src/salty.cr
+++ src/salty.cr
@@ -10,32 +10,49 @@ lib Sodium
   fun crypto_sign_ed25519_publickeybytes : UInt16
   fun crypto_sign_ed25519_secretkeybytes : UInt16
   fun crypto_sign_ed25519_bytes : UInt16
-  fun crypto_sign_ed25519_seed_keypair(UInt8*, UInt8*, UInt8*) : UInt8
+  fun crypto_sign_ed25519_seed_keypair(UInt8*, UInt8*, UInt8*) : LibC::Int
   fun randombytes_buf(UInt8*, UInt16)
-  fun crypto_sign_ed25519(UInt8*, UInt64*, UInt8*, UInt64, UInt8*)
+  fun crypto_sign_ed25519(UInt8*, UInt64*, UInt8*, UInt64, UInt8*) : LibC::Int
+  fun crypto_sign_ed25519_open(UInt8*, UInt64*, UInt8*, UInt64, UInt8*) : LibC::Int
+  fun crypto_sign_ed25519_verify_detached(UInt8*, UInt8*, UInt64, UInt8*) : LibC::Int
+  fun crypto_sign_check_S_lt_L(UInt8*) : LibC::Int
 end
 
 module Salty
   # TODO Put your code here
-  puts String.new(Sodium.sodium_version_string)
-  puts String.new(Sodium.crypto_generichash_primitive)
-  puts Sodium.crypto_generichash_blake2b_bytes_min
-  puts Sodium.crypto_generichash_blake2b_bytes_max
-  puts Sodium.crypto_sign_ed25519_seedbytes
-  puts Sodium.crypto_sign_ed25519_publickeybytes
-  puts Sodium.crypto_sign_ed25519_secretkeybytes
-  randseed = Pointer(UInt8).malloc(Sodium.crypto_sign_ed25519_seedbytes)
-  Sodium.randombytes_buf(randseed, Sodium.crypto_sign_ed25519_seedbytes)
-  pk = Pointer(UInt8).malloc(Sodium.crypto_sign_ed25519_publickeybytes)
-  sk = Pointer(UInt8).malloc(Sodium.crypto_sign_ed25519_secretkeybytes)
+  # puts String.new(Sodium.sodium_version_string)
+  # puts String.new(Sodium.crypto_generichash_primitive)
+  # puts Sodium.crypto_generichash_blake2b_bytes_min
+  # puts Sodium.crypto_generichash_blake2b_bytes_max
+  # puts Sodium.crypto_sign_ed25519_seedbytes
+  # puts Sodium.crypto_sign_ed25519_publickeybytes
+  # puts Sodium.crypto_sign_ed25519_secretkeybytes
+  msg = "Hello"
+  randseed = Slice(UInt8).new(Sodium.crypto_sign_ed25519_seedbytes)
   puts randseed
-  Sodium.crypto_sign_ed25519_seed_keypair(pk, sk, randseed)
-  puts pk
-  puts sk
-  # buffer = ("\0" * Sodium.crypto_sign_ed25519_bytes) + "Hello"
-  buffer = Pointer(UInt8).malloc(Sodium.crypto_sign_ed25519_bytes)
-  buf_len = Pointer(UInt64).malloc(64)
-  # puts Sodium.crypto_sign_ed25519_bytes
-  Sodium.crypto_sign_ed25519(buffer, buf_len, "Hello", "Hello".bytes.size, sk)
-  puts buffer
+  Sodium.randombytes_buf(randseed, Sodium.crypto_sign_ed25519_seedbytes)
+  puts randseed
+  pk = Slice(UInt8).new(Sodium.crypto_sign_ed25519_publickeybytes)
+  sk = Slice(UInt8).new(Sodium.crypto_sign_ed25519_secretkeybytes)
+  p Sodium.crypto_sign_ed25519_seed_keypair(pk, sk, randseed)
+  signature = Slice(UInt8).new(Sodium.crypto_sign_ed25519_bytes)
+  puts signature.bytesize
+  puts msg.bytesize
+  # msg.to_slice.copy_to signature
+  signature.copy_from msg.to_slice
+  puts signature
+  # puts String.new(signature)
+  puts signature.bytesize
+  # # buf_len = Pointer(UInt64).malloc(64)
+  buf_len = Slice(UInt64).new(64)
+  p Sodium.crypto_sign_ed25519(signature, buf_len, msg, msg.bytesize, sk)
+  puts signature
+  # puts String.new(signature)
+  puts signature.bytesize
+  # buf_len = Slice(UInt64).new(64)
+  sm = String.new(signature) + msg
+  puts sm
+  # # buffer = Pointer(UInt8).malloc(sm.bytesize)
+  buffer = Slice(UInt8).new(sm.bytesize)
+  p Sodium.crypto_sign_ed25519_open(buffer, buf_len, sm, sm.bytesize, pk)
 end
